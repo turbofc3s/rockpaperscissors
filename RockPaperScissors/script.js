5b2a2ee777ef55b23100d39066e7d41ec0656118
jeffrey burke <burkejeffrey56@yahoo.com>
2022-05-09 21:15:53 -0400

    First draft. Initial code written in. needs to be tweaked

---
+++ RockPaperScissors/script.js
@@ -0,0 +1,30 @@
+/* winning conditions needed
+variable to store number of wins for computer
+variable to store number of wins for user
+*/
+
+let rockWin = rock > paper || rock > 
+
+function rockFunction() {
+	if (rock = paper) {
+		document.getElementById('winner').innerHTML = You have lost the round!
+} else (rock = scissors) {
+		document.getElementById('winner').innerHTML = You have won the round!
+}
+}
+function paperFunction( ) {
+	if (paper = rock) {
+		document.getElementById('winner').innerHTML = You have won the roun
+		d!
+} else (paper = scissors)
+		document.getElementById('winner').innerHTML = You have lost the round!
+	}
+
+function scissorsFunction() {
+	if (scissors = rock) {
+		document.getElementById('winner').innerHTML = You have lost the roun
+		d!
+} else (scissors = paper)
+		document.getElementById('winner').innerHTML = You have won the round!
+	}
+}
